99e6f0d2043bd726f8a15e75adf52a20fb4f2042
Pavi Dhiman <pavidhiman22@gmail.com>
2025-07-16 22:33:25 -0400

    hyperparams and readme notes

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ Training an RL agent with Proximal Policy Optimization (PPO) from scratch using 
 **PPO**
 - Classic RL algorithm using exploration 
 - The agent collects data overtime through observations, actions and rewards and calculates the advantages. Ultimately, it updates the policy slightly using a clipped objective (allows it to stay close to the old policy to prevent large jumps)
+
+**Actor-Critic Network**
+- Actor: decides what to do and outputs an action (policy) (`action_logits = actor(obs)`)
+- The actor outputs a probability distribution over actions and learns the best policy to maximize the reward. In PPO it selects actions and updates the policy using the clipped loss
+- Critic: judges how good the action was and thus, estimates a value (`value = critic(obs)`)
+- The critic outputs a single number/value estimate of the expected future reward from that state. In PPO is calculates the advantage (how much better an action is than the average) 
---
+++ main.py
@@ -0,0 +1,25 @@
+import gymnasium as gym
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+    
+# hyperparams 
+ENV_ID = "CartPole-v1"      
+TOTAL_STEPS = 100_000       # total num of env steps before training stops 
+ROLLOUT_LEN = 2048          # num of steps collected before each policy update (ie, rollout/batch) - 2048 = power of 2, standard in PPO
+UPDATE_EPOCHS = 10          # num of times to reuse same rollout batch to update network
+GAMMA = 0.99                # discount factor for future rewards - care 99% about future rewards     
+LAMBDA = 0.95               # smoothing factor for Generalized Advantage Estimation (GAE) - (how good the action is - how good I thought it would be)
+CLIP_EPS = 0.2              # clip param - limits how much policy can change per step    
+LR = 3e-4                   # adam optimizer learning rate (ie, how big are updates to models weight)
+ENTROPY_COEF = 0.0          # encourages exploration     
+VALUE_COEF = 0.5             
+BATCH_SIZE = 64             # size of mini batc during gradient descent per rollout (2048/32 = 64) 
+SEED = 42                   # random num generators 
